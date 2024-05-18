91d861e7199475a099f6d8eb35dbe6af4827bc2c
nliaquin <nickolasiaquinta@gmail.com>
2024-05-18 19:42:56 -0400

    Updating blocks.def.h to remove laptop features

--- blocks.def.h
+++ blocks.def.h
@@ -3,9 +3,6 @@ static const Block blocks[] = {
     /*Icon*/    /*Command*/    /*Update Interval*/    /*Update Signal*/
     {"", "sh -c 'output=$(progress -q); if [ \"$output\" != \"No command currently running\" ]; then echo \"$output\" | head -n 2 | paste -sd \" \"; else echo \"\"; fi'", 1, 12},
     {"RAM:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 1, 0},
-    {"BRI:", "echo $((100 * $(brightnessctl get) / $(brightnessctl max)))%", 1, 11},
-    {"BAT:", "cat /sys/class/power_supply/BAT0/capacity", 30, 0},
-    {"", "cat /sys/class/power_supply/BAT0/status", 5, 0},
     {"", "date '+%D %R'", 5, 0},
 };
 
